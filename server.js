d1b21d4d9de866ad2c7661cc3d566e6d9c825a73
lodnanova-m <525221@mail.muni.cz>
2024-02-26 17:29:40 +0100

    feat: server update

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const fs = require("fs");
 const cors = require("cors");
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Cors configuration - Allows requests from localhost:4200
 const corsOptions = {
   origin: "http://localhost:4200",
   optionsSuccessStatus: 204,
-  methods: "GET, POST, PUT, DELETE",
+  methods: "GET",
 };
 
 // Use cors middleware
@@ -19,10 +19,8 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 // GET route - Allows to get all the items
-// example: localhost:3000/clothes?page=0&perPage=2
-app.get("/clothes", (req, res) => {
-  const page = parseInt(req.query.page) || 0;
-  const perPage = parseInt(req.query.perPage) || 10;
+// example: localhost:3000/clothes
+app.get("/products", (req, res) => {
 
   fs.readFile("db.json", "utf8", (err, data) => {
     if (err) {
@@ -32,124 +30,17 @@ app.get("/clothes", (req, res) => {
     }
 
     const jsonData = JSON.parse(data);
-
-    const start = page * perPage;
-    const end = start + perPage;
-
-    const result = jsonData.items.slice(start, end);
 
     res.status(200).json({
-      items: result,
-      total: jsonData.items.length,
-      page,
-      perPage,
-      totalPages: Math.ceil(jsonData.items.length / perPage),
+      items: jsonData.products,
+      total: jsonData.products.length,
     });
   });
 });
 
-// POST route - Allows to add a new item
-// example: localhost:3000/clothes
-/*
-  body: {
-    "image": "https://your-image-url.com/image.png",
-    "name": "T-shirt",
-    "price": "10",
-    "rating": 4
-  }
-*/
-app.post("/clothes", (req, res) => {
-  const { image, name, price, rating } = req.body;
-
-  fs.readFile("db.json", "utf8", (err, data) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send("Internal Server Error");
-      return;
-    }
-
-    const jsonData = JSON.parse(data);
-
-    const maxId = jsonData.items.reduce(
-      (max, item) => Math.max(max, item.id),
-      0
-    );
-
-    const newItem = {
-      id: maxId + 1,
-      image,
-      name,
-      price,
-      rating,
-    };
-
-    jsonData.items.push(newItem);
-
-    fs.writeFile("db.json", JSON.stringify(jsonData), (err) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send("Internal Server Error");
-        return;
-      }
-
-      res.status(201).json(newItem);
-    });
-  });
-});
-
-// PUT route - Allows to update an item
+// GET route - Allows to get a specific item
 // example: localhost:3000/clothes/1
-/*
-  body: {
-    "image": "https://your-image-url.com/image.png",
-    "name": "T-shirt",
-    "price": "10",
-    "rating": 4
-  }
-*/
-app.put("/clothes/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const { image, name, price, rating } = req.body;
-
-  fs.readFile("db.json", "utf8", (err, data) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send("Internal Server Error");
-      return;
-    }
-
-    const jsonData = JSON.parse(data);
-
-    const index = jsonData.items.findIndex((item) => item.id === id);
-
-    if (index === -1) {
-      res.status(404).send("Not Found");
-      return;
-    }
-
-    jsonData.items[index] = {
-      id,
-      image,
-      name,
-      price,
-      rating,
-    };
-
-    fs.writeFile("db.json", JSON.stringify(jsonData), (err) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send("Internal Server Error");
-        return;
-      }
-
-      res.status(200).json(jsonData.items[index]);
-    });
-  });
-});
-
-// DELETE route - Allows to delete an item
-// example: localhost:3000/clothes/1
-app.delete("/clothes/:id", (req, res) => {
+app.get("/products/:id", (req, res) => {
   const id = parseInt(req.params.id);
 
   fs.readFile("db.json", "utf8", (err, data) => {
@@ -161,23 +52,9 @@ app.delete("/clothes/:id", (req, res) => {
 
     const jsonData = JSON.parse(data);
 
-    const index = jsonData.items.findIndex((item) => item.id === id);
-
-    if (index === -1) {
-      res.status(404).send("Not Found");
-      return;
-    }
-
-    jsonData.items.splice(index, 1);
-
-    fs.writeFile("db.json", JSON.stringify(jsonData), (err) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send("Internal Server Error");
-        return;
-      }
-
-      res.status(204).send();
+    res.status(200).json({
+      item: jsonData.products.find((item) => item.id === id),
+      total: 1,
     });
   });
 });
